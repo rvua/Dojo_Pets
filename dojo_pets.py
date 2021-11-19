c2f9de09404bab2ac354983f9736f986e553dfcb
rvua <rvanore26@gmail.com>
2021-11-19 13:31:11 -0600

    corrections

--- dojo_pets.py
+++ dojo_pets.py
@@ -1,49 +1,75 @@
-class Ninja:
-    def __init__(self, first_name, last_name, pet, treats, pet_food):
-      self.first_name = first_name
-      self.last_name = last_name
-      self.pet = pet
-      self.treats = treats
-      self.pet_food = pet_food
+class Pet:
 
-    def walk(self):
-        print(f"Taking {self.first_name} for a walk.")
-
-    def feed(self):
-        print(f"Time to feed {self.first_name} her {self.pet_food}.") 
-
-    def bathe(self):
-        print(f"Time to bathe {self.first_name}.")
-
-class Pet: 
-    def __init__(self, name, type, tricks, health, energy):
-        self.name = name 
+    # implement __init__( name , type , tricks ):
+    def __init__(self, name , type, tricks, noise):
+        self.name = name
         self.type = type
         self.tricks = tricks
-        self.health = health
-        self.energy = energy 
-        self.ninja = Ninja('sally', 'jones', 'dog', 'bone', 'kibble')
-    
+        self.health = 100
+        self.energy = 50
+        self.noise = noise
+
+    # implement the following methods:
+    # sleep() - increases the pets energy by 25
     def sleep(self):
-        print(f"Put {self.name} to sleep.")
+        self.energy += 25
+        return self
 
+    # eat() - increases the pet's energy by 5 & health by 10
     def eat(self):
-        self.ninja.feed()
-        print(self.ninja.pet_food)
+        self.energy += 5
+        self.health += 10
+        return self
 
+    # play() - increases the pet's health by 5
     def play(self):
-        self.ninja.walk()
-        print(f"Take {self.name} for a walk and give her a {self.ninja.treats}")
+        self.health += 5
+        self.energy -= 15
+        return self
 
+    # noise() - prints out the pet's sound
     def noise(self):
-        self.ninja
-        print(f"{self.name} is very {self.energy} right now. Please give her a {self.ninja.treats}")
+        print(self.noise)
 
-sally = Pet('sally', 'dog', 'shakes', 'healthy', 'hyper') 
-ninja = Ninja('sally', 'jones', 'dog', 'bone', 'kibble')
-ninja.feed()
-ninja.walk()
-ninja.bathe()
-sally.eat()
-sally.play()
-sally.noise()
+
+
+class Ninja:
+    # implement __init__( first_name , last_name , treats , pet_food , pet )
+    def __init__(self, first_name, last_name , treats, pet_food, pet):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.treats = treats
+        self.pet_food = pet_food
+        self.pet = pet
+
+    # implement the following methods:
+    # walk() - walks the ninja's pet invoking the pet play() method
+    def walk(self):
+        self.pet.play()
+        return self
+
+    # feed() - feeds the ninja's pet invoking the pet eat() method
+    def feed(self):
+
+        if len(self.pet_food) > 0:
+            food = self.pet_food.pop()
+            print(f"Feeding {self.pet.name} {food}!")
+            self.pet.eat()
+        else:
+            print("Oh no!!! you need more pet food!")
+        return self
+
+    # bathe() - cleans the ninja's pet invoking the pet noise() method
+    def bathe(self):
+        self.pet.noise()
+
+my_treats = ['Snausage','Bacon',"Trash Bag"]
+my_pet_food = ['Pizza','Burger']
+
+nibbles = Pet("Mr. Nibbles","Horse",['nibbles on things','is invisible'],"Hey Hey")
+
+adrien = Ninja("Adrien","Dion",my_treats,my_pet_food, nibbles)
+
+adrien.feed();
+adrien.feed();
+adrien.feed();
